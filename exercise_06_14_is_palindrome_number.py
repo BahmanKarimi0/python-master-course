675439086dabf59badad639e78ca69f370e5d4fe
BahmanKarimi0 <bahmankarimi0@gmail.com>
2025-07-23 02:17:20 +1200

    Delete exercise_06_14_is_palindrome_number.py

--- exercise_06_14_is_palindrome_number.py
+++
@@ -1,9 +0,0 @@
-while not (num := input("Enter a number: ")).isdigit():
-    print("⚠️  Please enter a positive integer greater than 0.")
-
-
-def is_palindrome(n):
-    return n == n[::-1]
-
-
-print(f'✅ Is palindrome: {is_palindrome(num)}')
