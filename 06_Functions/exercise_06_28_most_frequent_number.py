fd21fefe3bd53509904e408998619dc543d02081
BahmanKarimi0 <bahmankarimi0@gmail.com>
2025-07-27 05:52:19 +1200

    Add exercise 06-28 solution

---
+++ 06_Functions/exercise_06_28_most_frequent_number.py
@@ -0,0 +1,25 @@
+while True:
+    nums = input('Enter list of numbers that separate with space: ').strip().split()
+    exist = True
+    if nums:
+        for num in nums:
+            if not num.isdigit():
+                print("⚠️  Please enter a integer number rather that 0.")
+                exist = False
+                break
+    else:
+        print('⚠️  list cannot be empty')
+        exist = False
+    if exist:
+        numbers = [int(num) for num in nums]
+        break
+
+
+def most_frequent(lst):
+    count_number = {}
+    for num in lst:
+        count_number[num] = count_number.get(num, 0) + 1
+    return max(count_number.items(), key=lambda x: x[1])
+
+
+print(f'Number: {most_frequent(numbers)[0]}, Count: {most_frequent(numbers)[1]}')
