dfcc15c7e24419b047731366aa3fb4d3dae6ab81
BahmanKarimi0 <bahmankarimi0@gmail.com>
2025-08-09 07:18:53 +1200

    Add exercise 06-68 solution

---
+++ 06_Functions/exercise_06_68_advanced_filter.py
@@ -0,0 +1,46 @@
+def advanced_filter(keyword, min_length, target):
+    msg_count = 0
+    passed_count = 0
+    try:
+        while True:
+            msg = yield
+            msg_count += 1
+            if not isinstance(msg, str):
+                print(f"⚠️ Invalid input type: expected str, got {type(msg).__name__} ({msg!r})")
+            elif msg.casefold().find(keyword.casefold()) != -1 and len(msg) > min_length:
+                passed_count += 1
+                target.send(msg)
+    except GeneratorExit:
+        print(f'Total messages received: {msg_count}')
+        print(f'Messages passed filter: {passed_count}')
+        target.close()
+
+
+def message_logger():
+    try:
+        while True:
+            msg = yield
+            print(f'Logged: {msg}')
+    except GeneratorExit:
+        print('🔒 Coroutine closed gracefully.')
+
+
+log = message_logger()
+next(log)
+
+filterer = advanced_filter('error', 10, log)
+next(filterer)
+
+
+messages = [
+    "Error 404",           # رد می‌شود (کمتر از 10 کاراکتر)
+    "Critical error found",# قبول می‌شود
+    "Warning: Disk full",  # رد می‌شود (کلمه کلیدی نیست)
+    "Major error detected" # قبول می‌شود
+]
+
+for msg in messages:
+    filterer.send(msg)
+
+filterer.close()
+
